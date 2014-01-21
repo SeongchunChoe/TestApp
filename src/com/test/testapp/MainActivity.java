2b3d86c2880a956a4b466dfa3c41449f38558fa6
seongchun.choe <chsuch@naver.com>
2014-01-21 10:26:42 +0900

    로그 추가

--- src/com/test/testapp/MainActivity.java
+++ src/com/test/testapp/MainActivity.java
@@ -2,14 +2,18 @@ package com.test.testapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private static String TAG = MainActivity.class.getSimpleName();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Log.d(TAG, "Add log");
 	}
 
 	@Override
